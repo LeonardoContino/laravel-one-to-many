5018e8658c07635cde37a128ebd6bf7de23990a7
LeonardoContino <leoconti2000@hotmail.it>
2023-03-10 15:58:25 +0100

    add migration

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Generator $faker): void
+    {
+        $labels = ['frontEnd', 'Backend', 'FullStack', 'UX/UI', 'ProjectDesign'];
+
+        foreach($labels as $label){
+            $type = new Type();
+            $type->label = $label;
+            $type->color = $faker->hexColor();
+            $type->save();
+        }
+    }
+}
